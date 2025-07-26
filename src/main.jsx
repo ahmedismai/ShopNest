9887d320d621b7c451bd83535eb560660e1a3925
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-26 03:10:07 +0300

    fix: corrected import path for CartContext in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import CartContextProvider from './context/CartContext'
+import CartContextProvider from './Context/cartContext'
 
 
 const queryClient = new QueryClient()
